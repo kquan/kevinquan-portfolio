43270c2a59c9b869eb93374fba9b569b00b22722
Kevin Quan <kevin.quan@gmail.com>
2014-03-07 22:46:21 -0500

    first release

---
+++ web/js/animations.js
@@ -0,0 +1,45 @@
+// Animate Condado stats in
+$(window).scroll(function() {	
+	var scrollTop = $(window).scrollTop() + $(window).height();
+	var condadoStatsTop = $('#condado-stats').offset().top;
+ 
+	if(scrollTop >= condadoStatsTop && !$('#condado-stats li:nth-child(1)').hasClass('condado-stat-visible')) {
+		$('#condado-stats li:nth-child(1)').addClass('condado-stat-visible');
+		$({countNum:0}).animate({countNum:30000}, {
+			duration: 2000,
+			easing: 'linear',
+			step: function() {
+				$('#condado-downloads').text(Math.floor(this.countNum)+"+");
+			},
+			complete: function() {
+				$('#condado-downloads').text("30000+");
+			}
+		});
+		setTimeout(function() {
+			$('#condado-stats li:nth-child(2)').addClass('condado-stat-visible');
+			$({countNum:0}).animate({countNum:47}, {
+				duration: 1000,
+				easing: 'linear',
+				step: function() {
+					$('#condado-rating').text(Math.floor(this.countNum)/10+"/5");
+				},
+				complete: function() {
+					$('#condado-rating').text("4.7/5");
+				}
+			});
+		}, 200);
+		setTimeout(function() {
+			$('#condado-stats li:nth-child(3)').addClass('condado-stat-visible');
+			$({countNum:0}).animate({countNum:700}, {
+				duration: 1500,
+				easing: 'linear',
+				step: function() {
+					$('#condado-reviews').text(Math.floor(this.countNum)+"+");
+				},
+				complete: function() {
+					$('#condado-reviews').text("700+");
+				}
+			});
+		}, 400);
+	}
+});
